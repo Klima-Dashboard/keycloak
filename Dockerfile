be743cb42f97d9aae0690fea413e96977a82ca3a
Mats Heemeyer <meytin@protonmail.com>
2023-05-12 09:22:32 +0200

    Switch to mariadb

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,9 @@
 FROM quay.io/keycloak/keycloak:21.1
 COPY entrypoint.sh /entrypoint.sh
+# Enable health and metrics support
+ENV KC_HEALTH_ENABLED=true
+ENV KC_METRICS_ENABLED=true
+
+# Configure a database vendor
+RUN /opt/keycloak/bin/kc.sh build --db=mariadb
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 export KEYCLOAK_ADMIN_PASSWORD=$(cat /run/secrets/keycloak-admin-password) 
-export KC_DB_PASSWORD=$(cat /run/secrets/postgres-password)
-./opt/keycloak/bin/kc.sh start --proxy=edge --optimized
+export KC_DB_PASSWORD=$(cat /run/secrets/mariadb-keycloak-password)
+./opt/keycloak/bin/kc.sh start --proxy=edge --optimized --spi-connections-jpa-legacy-migration-strategy=update
