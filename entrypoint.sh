ef2e0cfad79c825052e2628d13f4fed20fc085b4
Mats Heemeyer <meytin@protonmail.com>
2024-02-12 13:36:15 +0100

    Add deployment for michendorf

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-export KEYCLOAK_ADMIN_PASSWORD=$(cat /run/secrets/keycloak-admin-password) 
-export KC_DB_PASSWORD=$(cat /run/secrets/mariadb-keycloak-password)
+export KEYCLOAK_ADMIN_PASSWORD=$(cat /run/secrets/michendorf-keycloak-admin-password) 
+export KC_DB_PASSWORD=$(cat /run/secrets/michendorf-mariadb-keycloak-password)
 ./opt/keycloak/bin/kc.sh start --proxy=edge --optimized --spi-connections-jpa-legacy-migration-strategy=update
